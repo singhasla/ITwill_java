19643fed0a637d99ff76a3e5dc69a36fd09d94ec
ITWILL <ITWILL@DESKTOP-KJ97GP7>
2020-09-11 11:39:58 +0900

    클래스 선언 & 객체 생성

---
+++ javaBook/src/ch06/P219_StudentExample.java
@@ -0,0 +1,13 @@
+package ch06;
+
+public class P219_StudentExample {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println("s1 변수가 Student 객체를 참조합니다. "+ s1);
+		
+		Student s2 = new Student();
+		System.out.println("s2 변수가 또 다른 Student 객체를 참조합니다. " + s2);
+	}
+
+}
