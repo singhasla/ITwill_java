678abc30d564ddaa88fcb4307618067bbfdebb61
singhasla <s1n9hasla@gmail.com>
2020-09-18 11:59:14 +0900

    상수 선언 및 사용

---
+++ javaBook/src/ch06/P283_Earth.java
@@ -0,0 +1,7 @@
+package ch06;
+
+public class P283_Earth {
+
+	static final double EARTH_RADIUS = 6400;
+	static final double EARTH_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+}
---
+++ javaBook/src/ch06/P283_EarthExample.java
@@ -0,0 +1,11 @@
+package ch06;
+
+public class P283_EarthExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("지구의 반지름: " + P283_Earth.EARTH_RADIUS + " km");
+		System.out.println("지구의 표면적: " + P283_Earth.EARTH_AREA + " km^2");
+	}
+
+}
