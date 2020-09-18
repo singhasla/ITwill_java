61e430d27e5d24adaba8f7f98da6e58ffab96a70
singhasla <s1n9hasla@gmail.com>
2020-09-18 11:57:30 +0900

    메소드 오버로딩 연습문제

---
+++ javaBook/src/ch06/P269_PrinterExample.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class P269_PrinterExample {
+
+	public static void main(String[] args) {
+	
+		P269_Printer printer = new P269_Printer();
+		
+		printer.println(10);
+		printer.println(true);
+		printer.println(5.7);
+		printer.println("ȫ�浿");
+
+	}
+
+}
