25a6d3dd08c0f877ad52684342ca48f38509a963
singhasla <s1n9hasla@gmail.com>
2020-09-18 13:00:31 +0900

    Getter와 Setter메소드 알아보기

---
+++ javaBook/src/ch07_diff/exam_Getter_Setter.java
@@ -0,0 +1,18 @@
+package ch07_diff;
+
+public class exam_Getter_Setter {
+
+	private int balance;
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		if(balance>=0)
+			this.balance = balance;
+		else
+			System.out.println("정상적인 값이 아닙니다.");
+	}
+	
+}
