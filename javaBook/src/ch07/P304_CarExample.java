88c0f2c941cba635a19e978dbaa9c2ea9da3b720
singhasla <s1n9hasla@gmail.com>
2020-09-18 14:05:01 +0900

    getter와 setter 메소드 선언 및 사용2

---
+++ javaBook/src/ch07/P304_CarExample.java
@@ -0,0 +1,25 @@
+package ch07;
+
+public class P304_CarExample {
+
+	public static void main(String[] args) {
+
+		P304_Car myCar = new P304_Car();
+		
+		//잘못된 속도 변경
+		myCar.setSpeed(-50);
+		
+		System.out.println("현재 속도: " + myCar.getSpeed());
+		
+		//올바른 속도 변경
+		myCar.setSpeed(60);
+		
+		//멈춤
+		if(!myCar.isStop()) {
+			myCar.setStop(true);
+		}
+		
+		System.out.println("현재 속도: " + myCar.getSpeed());
+	}
+
+}
