d23c654c8348a3c5921d8cf0cdc9b8cf9cacddf3
singhasla <s1n9hasla@gmail.com>
2020-09-25 11:33:35 +0900

    추상 클래스 선언

---
+++ javaBook/src/ch07/P361_PhoneExample.java
@@ -0,0 +1,16 @@
+package ch07;
+
+public class P361_PhoneExample {
+
+	public static void main(String[] args) {
+
+		//P360_Phone phone = new P360_Phone();
+		
+		P361_SmartPhone smartPhone = new P361_SmartPhone("ȫ�浿");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+	}
+
+}
---
+++ javaBook/src/ch07/P361_SmartPhone.java
@@ -0,0 +1,14 @@
+package ch07;
+
+public class P361_SmartPhone extends P360_Phone{
+
+	//생성자
+	public P361_SmartPhone(String owner) {
+		super(owner);
+	}
+	
+	//메소드
+	public void internetSearch() {
+		System.out.println("인터넷 검색을 합니다.");
+	}
+}
