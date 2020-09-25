2f0d06e8e49a35a9ddfbcbdde1ee64c7c77744cd
singhasla <s1n9hasla@gmail.com>
2020-09-25 11:33:12 +0900

    P367 연습문제

---
+++ javaBook/src/ch07/FileDownloadServlet.java
@@ -0,0 +1,8 @@
+package ch07;
+
+public class FileDownloadServlet extends P367_Q3_HttpServlet{
+	@Override
+	public void service() {
+		System.out.println("파일 다운로드합니다.");
+	}
+}
---
+++ javaBook/src/ch07/LoginServlet.java
@@ -0,0 +1,8 @@
+package ch07;
+
+public class LoginServlet extends P367_Q3_HttpServlet{
+	@Override
+	public void service() {
+		System.out.println("로그인합니다.");
+	}
+}
