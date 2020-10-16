f7382d16968ee3cf575c0025a970c1d832b82fca
singhasla <s1n9hasla@gmail.com>
2020-10-16 11:04:17 +0900

    String 메소드

---
+++ javaBook/src/ch11/P498_StringValueOfExam.java
@@ -0,0 +1,17 @@
+package ch11;
+
+public class P498_StringValueOfExam {
+
+	public static void main(String[] args) {
+
+		String str1 = String.valueOf(10);
+		String str2 = String.valueOf(10.5);
+		String str3 = String.valueOf(true);
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);		
+
+	}
+
+}
