d0d1f07d2607bf47638f7abc33bcccb1bb21c770
singhasla <s1n9hasla@gmail.com>
2020-10-19 09:55:02 +0900

    Math.random()메소드 활용

---
+++ javaBook/src/ch11/P507_MathRandomExam.java
@@ -0,0 +1,11 @@
+package ch11;
+
+public class P507_MathRandomExam {
+
+	public static void main(String[] args) {
+
+		int num = (int)(Math.random()*6) + 1;
+		System.out.println("ÁÖ»çÀ§ ´«: " + num);
+	}
+
+}
