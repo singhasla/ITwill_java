657cbfd6cc3174b28eb700c78664fe6d253a9f9b
singhasla <s1n9hasla@gmail.com>
2020-10-19 09:52:45 +0900

    Wrapper(포장)클래스

---
+++ javaBook/src/ch11/P502_StringToPrimitiveValueExam.java
@@ -0,0 +1,17 @@
+package ch11;
+
+public class P502_StringToPrimitiveValueExam {
+
+	public static void main(String[] args) {
+
+		int value1 = Integer.parseInt("10");
+		double value2 = Double.parseDouble("3.14");
+		boolean value3 = Boolean.parseBoolean("true");
+		
+		System.out.println("value1: " + value1);
+		System.out.println("value2: " + value2);
+		System.out.println("value3: " + value3);
+		
+	}
+
+}
