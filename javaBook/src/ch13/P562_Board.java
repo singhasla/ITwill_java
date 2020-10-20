21fb18daa0c8ea32b0eed02f1185321fafafc240
singhasla <s1n9hasla@gmail.com>
2020-10-20 13:55:35 +0900

    List 컬렉션

---
+++ javaBook/src/ch13/P562_Board.java
@@ -0,0 +1,16 @@
+package ch13;
+
+public class P562_Board {
+
+	String subject;
+	String content;
+	String writer;
+	
+	public P562_Board(String subject, String content, String writer) {
+		this.subject = subject;
+		this.content = content;
+		this.writer = writer;
+	}
+	
+	
+}
