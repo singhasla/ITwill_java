c6f3ea48fb5a6eeed0c41c239d3638b4d8f07835
singhasla <s1n9hasla@gmail.com>
2020-10-27 09:44:50 +0900

    P584 연습문제

---
+++ javaBook/src/ch13/P584_MapExample.java
@@ -0,0 +1,41 @@
+package ch13;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class P584_MapExample {
+
+	public static void main(String[] args) {
+
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("blue", 96);
+		map.put("hong", 86);
+		map.put("white", 92);
+		
+		String name = null;
+		int maxScore = 0;
+		int totalScore = 0;
+		
+		Set<String> keySet = map.keySet();
+		Iterator<String> keyIterator = keySet.iterator();
+		
+		while(keyIterator.hasNext()) {
+			String key = keyIterator.next();
+			Integer value = map.get(key);
+			
+			if(value>maxScore) {
+				name = key;
+				maxScore = value;
+			}
+			totalScore += value;
+			
+		}
+		System.out.println("평균점수: " + totalScore / map.size());
+		System.out.println("최고점수: " + maxScore);
+		System.out.println("최고점수를 받은 아이디: " + name);
+		
+	}
+
+}
